1290fc6dfc3256fd3b0143a8c95394e2e0fc6d12
gustavogarciam <83886180+gustavogarciam@users.noreply.github.com>
2022-03-28 09:31:54 -0300

    Add files via upload

---
+++ 03 - 54 - GBPBRL spectrum.R
@@ -0,0 +1,106 @@
+setwd("D:/dataTCC/4-GBPBRL")
+
+library(ggplot2)
+library(reshape2)
+library(latex2exp)
+
+data.gbp <- read.csv("GBPBRL.csv",  header = FALSE)
+
+price <- data.gbp$V3
+X.gbp <- log(price) - log(price[1])
+
+
+# Partition Function
+library(Rcpp)
+Rcpp::cppFunction('
+
+double Sq(NumericVector X, double q, int dt) {
+  long double total = 0;
+  int n = X.size();
+  int k = 0;
+  while (k + dt <= n-1) {
+    total += pow(abs(X[k+dt] - X[k]), q);
+    k += dt;
+  }
+  total += pow(abs(X[n-1] - X[k]), q);
+  return total;
+}
+
+')
+
+# Scale Function
+tau <- function(qs, X, dt){
+  tq <- NULL
+  i <- 1
+  for(q in qs){
+    teste <- NULL
+    j <- 1
+    for(t in dt){
+      teste[j] <- Sq(X, q, t)
+      j <- j+1
+    }
+    reg <- lm(log(teste) ~ log(dt))
+    tq[i] <- reg$coefficients[2]
+    i <- i+1
+    print(q) # comment if you don't want to see progression
+  }
+  result <- data.frame(q = qs, tau_q = tq)
+  return(result)
+}
+
+q <- seq(0, 5, 0.05)
+dt <- 10:20000
+tauq <- tau(q,X.gbp,dt)
+
+(H <- 2.156^-1) # dt <- 10:20000
+
+reg_tau <- lm(tauq$tau_q ~ poly(tauq$q,2, raw=T))
+regfit <- data.frame(q = q, tauadj = predict(reg_tau))
+
+b0 <- reg_tau$coefficients[1]
+b1 <- reg_tau$coefficients[2]
+b2 <- reg_tau$coefficients[3]
+
+a <- seq(0, 1, 0.01)
+fa <- -b0 + ((b1-a)^2)/(4*b2)
+espectro <- data.frame(alpha = a, f = fa)
+
+g1 <- ggplot(data=tauq,
+       aes(x=q, y=tau_q)) +
+  #geom_path()+
+  geom_point()+
+  theme_bw()+
+  ylab(TeX(r'($\tau (q)$)'))+
+  xlab(TeX(r'($q$)'))+
+  ggtitle("Função de escala - câmbio BRL/GBP")+
+  geom_line(color='red',data = regfit, aes(x=q, y=tauadj))
+g1
+
+g2 <- ggplot(data=espectro,
+             aes(x=alpha, y=f)) +
+  ylim(-0.001,1)+
+  geom_line(color = 'red')+
+  #geom_point()+
+  theme_bw()+
+  ylab(TeX(r'($f(\alpha)$)'))+
+  xlab(TeX(r'($\alpha$)'))+
+  ggtitle("Espectro Multifractal - câmbio BRL/GBP")
+
+g2
+
+library(gridExtra)
+grid.arrange(g1, g2, nrow=2)
+
+
+(alpha0 <- b1)
+(lambda <- b1/H)
+(sigma2 <- 2*(lambda-1)/log(2))
+
+# sanity check
+b0 - (-1)
+b2 - (-H*(b1-H))
+
+# sanity check: EM = 1/b?
+set.seed(0)
+M <- rlnorm(10^6, -log(2)*lambda, log(2)*(sigma2^0.5))
+mean(M) - 1/2 # correto
---
+++ 04 - 54 - MMAR coefficients estimation.R
@@ -0,0 +1,232 @@
+# -----------
+# Pacotes
+# -----------
+
+library(ggplot2)
+library(latex2exp)
+library(Rcpp)
+library(gridExtra)
+library(somebm)
+library(readr)
+library(dplyr)
+library(lubridate)
+
+# ----------------
+# Leitura de dados
+# ----------------
+
+### USDDM ###
+
+USDDM <- read_csv("C:/Users/Murilo/Dropbox/TCC/códigos/função de partição Fisher 1997 USDDM e USDBRL/USDDM.csv")
+
+# Appendix 9.3: 6119 observations for weekdays from 1973-06-04 to 1996-12-31
+# Except Christmas and New Year's Day
+
+USDDM$weekday <- wday(USDDM$Date) # 7 - Saturday, 1 - Sunday
+
+USDDM <- USDDM %>% filter(weekday != 7)
+USDDM <- USDDM %>% filter(weekday != 1)
+
+isChristmas <- month(USDDM$Date) == 12 & day(USDDM$Date) == 25
+USDDM <- USDDM[!isChristmas,]
+isNewYear <- month(USDDM$Date) == 1 & day(USDDM$Date) == 1
+USDDM <- USDDM[!isNewYear,]
+
+# 6119 observations, as desired
+# Appendix 9.3, JPY/USD data: results not substantially different when
+# i) replacing missing values with previous day's price or ii) eliminating missing values
+# Here it is assumed the same applies to DM/USD data
+
+for(i in 2:nrow(USDDM)){
+  ifelse(is.na(USDDM[i,2]), USDDM[i,2] <- USDDM[i-1,2], 1)
+}
+
+price <- USDDM$USDDM
+
+dt <- 1:180
+# H <- 1.905^-1
+# alpha0 <- 0.558692
+# lambda <- 1.064308
+# sigma2 <- 0.1855545
+# s <- sqrt(sigma2)
+
+### USDBRL diário ###
+
+USDBRL <- read_csv("C:/Users/Murilo/Dropbox/TCC/códigos/MMAR simulation/USDBRL.csv")
+price <- USDBRL$USDBRL
+
+dt <- 1:180
+# H <- 1.808^-1
+# alpha0 <- 0.6296443
+# lambda <- 1.138397
+# sigma2 <- 0.399329
+# s <- sqrt(sigma2)
+
+### USDBRL intraday ###
+
+USDBRL <- read_csv("D:/dataTCC/2-USDBRL/USDBRLt.csv", col_names = FALSE)
+price <- USDBRL$X3
+
+dt <- 100:62000
+# H <- 1.842^-1
+
+### USDCHF diário ###
+
+library(readr)
+USDCHF <- read_csv("D:/dataTCC/USDCHF/FRB_H10.csv")
+price <- na.omit(USDCHF$USDCHF)
+
+dt <- 1:150
+
+### USDCHF intraday ###
+
+setwd("D:/dataTCC/USDCHF")
+data.1 <- read.csv("DAT_MT_USDCHF_M1_2000.csv",  header = FALSE)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2001.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2002.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2003.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2004.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2005.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2006.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2007.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2008.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2009.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2010.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2011.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2012.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2013.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_2014.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_201501.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_201502.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_201503.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_201504.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_201505.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+data.a <- read.csv("DAT_MT_USDCHF_M1_201506.csv",  header = FALSE)
+data.1 <- rbind(data.1,data.a)
+
+price <- data.1$V3
+
+dt <- 100:180000
+# H <- 2.301^-1
+# alpha0 <- 0.5895289
+# lambda <- 1.356506
+# sigma2 <- 1.028659
+# s <- sqrt(sigma2)
+
+### EURBRL ###
+
+EURBRL <- read_csv("D:/dataTCC/3-EURBRL/EURBRL.csv", col_names = FALSE)
+price <- EURBRL$X3
+
+dt <- 10:20000
+# H <- 2.404^-1
+
+### GBPBRL ###
+
+GBPBRL <- read_csv("D:/dataTCC/4-GBPBRL/GBPBRL.csv", col_names = FALSE)
+price <- GBPBRL$X3
+
+dt <- 10:20000
+# H <- 2.156^-1
+
+  
+# -----------
+# Log-Retorno
+# -----------
+X <- log(price) - log(price[1]) # X(t) = ln(P(t)/P(0))
+
+# --------------------------------------------
+# Função de partição Sq e Função de escala tau
+# --------------------------------------------
+
+Rcpp::cppFunction('
+
+double Sq(NumericVector X, double q, int dt) {
+  long double total = 0;
+  int n = X.size();
+  int k = 0;
+  while (k + dt <= n-1) {
+    total += pow(abs(X[k+dt] - X[k]), q);
+    k += dt;
+  }
+  total += pow(abs(X[n-1] - X[k]), q);
+  return total;
+}
+
+')
+
+tau <- function(qs, X, dt){
+  tq <- NULL
+  i <- 1
+  for(q in qs){
+    teste <- NULL
+    j <- 1
+    for(t in dt){
+      teste[j] <- Sq(X, q, t)
+      j <- j+1
+    }
+    reg <- lm(log(teste) ~ log(dt))
+    tq[i] <- reg$coefficients[2]
+    i <- i+1
+    print(q) # comente para não ver a progressão
+  }
+  result <- data.frame(q = qs, tau_q = tq)
+  return(result)
+}
+
+tausimples <- function(q, X, dt){
+  fpart <- rep(0, length(dt))
+  i <- 1
+  for(t in dt){
+    fpart[i] <- Sq(X, q, t)
+    i <- i+1
+  }
+  reg <- lm(log(fpart) ~ log(dt))
+  return(reg$coefficients[2])
+}
+
+# ---------------
+# Estimação de H
+# ---------------
+
+H <- (uniroot(tausimples, interval = c(0, 5), X = X, dt = dt)$root)^-1
+
+# -------------------------------------------------------------------------
+# Ajuste do espectro multifractal f: estimação de alpha_0, lambda e sigma^2
+# -------------------------------------------------------------------------
+
+q <- seq(0, 5, 0.05)
+
+tauq <- tau(q,X,dt)
+
+reg_tau <- lm(tauq$tau_q ~ poly(tauq$q,2, raw=T))
+
+# Coeficientes da regressão
+b0 <- reg_tau$coefficients[1]
+b1 <- reg_tau$coefficients[2]
+b2 <- reg_tau$coefficients[3]
+
+# Parâmetros
+alpha0 <- b1
+lambda <- alpha0/H
+sigma2 <- 2*(lambda-1)/log(2)
+s <- sqrt(sigma2)
